5642beb6be1b219b3cb42db83e40618ae4ef36ab
Mario-Vivobook <mario_constantin1234@outlook.com>
2025-03-24 12:18:26 +0100

    Inital commit - set up TF quality gate

---
+++ terraform/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.107" # Pin AzureRM provider version
+    }
+  }
+}
+
+# Configure default provider
+provider "azurerm" {
+  features {}
+
+#   client_id       = var.client_id  
+#   client_secret   = var.client_secret
+#   tenant_id       = var.tenant_id
+#   subscription_id = var.subscription_id
+
+   client_id       =   
+   client_secret   = 
+   tenant_id       = 
+   subscription_id = 
+
+}
